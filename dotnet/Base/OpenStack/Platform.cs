ae35e214fd655a0dd7ee8bff6b871fe59d3bcf65
Sky Morey <apphaus@outlook.com>
2025-04-24 03:54:03 -0500

    updates

--- dotnet/Base/OpenStack/Platform.cs
+++ dotnet/Base/OpenStack/Platform.cs
@@ -12,6 +12,26 @@ using YamlDotNet.Serialization.NamingConventions;
 
 namespace OpenStack;
 
+/// <summary>
+/// BaseExtensions
+/// </summary>
+public static class BaseExtensions {
+    public static IEnumerable<string> FindPaths(this IFileSystem fileSystem, string path, string searchPattern) {
+        // expand
+        int expandStartIdx, expandMidIdx, expandEndIdx;
+        if ((expandStartIdx = searchPattern.IndexOf('(')) != -1 &&
+            (expandMidIdx = searchPattern.IndexOf(':', expandStartIdx)) != -1 &&
+            (expandEndIdx = searchPattern.IndexOf(')', expandMidIdx)) != -1 &&
+            expandStartIdx < expandEndIdx) {
+            foreach (var expand in searchPattern.Substring(expandStartIdx + 1, expandEndIdx - expandStartIdx - 1).Split(':'))
+                foreach (var found in FindPaths(fileSystem, path, searchPattern.Remove(expandStartIdx, expandEndIdx - expandStartIdx + 1).Insert(expandStartIdx, expand)))
+                    yield return found;
+            yield break;
+        }
+        foreach (var file in fileSystem.Glob(path, searchPattern)) yield return file;
+    }
+}
+
 #region FileSystem
 
 /// <summary>
