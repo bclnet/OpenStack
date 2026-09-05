86b4ab6e62b8f7533053f957d9aa61d7b65a48a2
Sky Morey <apphaus@outlook.com>
2026-09-05 13:02:37 -0500

    updates

--- dotnet/Gfx/OpenStack.Gfx.Egin/Egin.cs
+++
@@ -1,5 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("OpenStack.GfxTests")]
-
-namespace OpenStack.Gfx.Egin;
---
+++ dotnet/Gfx/OpenStk.Gfx.Egin/Egin.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("OpenStk.GfxTests")]
+
+namespace OpenStack.Gfx.Egin;
